73caaae14b0e60665bca3d2749975a4b1f1bf9f2
Nico <elnicopons@gmai.com>
2026-03-07 13:45:00 -0300

    First commit. Functional app

---
+++ src/app/constants/sessions.ts
@@ -0,0 +1,38 @@
+import { ValueText } from '../models/value-text';
+
+export const sets = [
+  { value: 1, text: '1' },
+  { value: 2, text: '2' },
+  { value: 3, text: '3' },
+  { value: 4, text: '4' },
+  { value: 5, text: '5' },
+  { value: 6, text: '6' },
+  { value: 7, text: '7' },
+  { value: 8, text: '8' },
+  { value: 9, text: '9' },
+  { value: 10, text: '10' },
+];
+
+export const intervals: ValueText[] = [
+  { value: 5, text: '5 seg' },
+  { value: 10, text: '10 seg' },
+  { value: 20, text: '20 seg' },
+  { value: 30, text: '30 seg' },
+  { value: 45, text: '45 seg' },
+  { value: 60, text: '1 min' },
+  { value: 90, text: '1.5 min' },
+  { value: 120, text: '2 min' },
+  { value: 300, text: '5 min' },
+];
+
+export const rests: ValueText[] = [
+  { value: 5, text: '5 seg' },
+  { value: 10, text: '10 seg' },
+  { value: 20, text: '20 seg' },
+  { value: 30, text: '30 seg' },
+  { value: 45, text: '45 seg' },
+  { value: 60, text: '1 min' },
+  { value: 90, text: '1.5 min' },
+  { value: 120, text: '2 min' },
+  { value: 300, text: '5 min' },
+];
